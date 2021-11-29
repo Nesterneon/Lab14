2021411cafcc048fa2d05916cf00d0ab1300c49f
79064 <79064@MSI>
2021-11-29 17:27:10 +0300

    Добавьте файлы проекта.

---
+++ Lab14/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+            dog.ShowInfo();
+            dog.Say();
+            cat.ShowInfo();
+            cat.Say();
+            Console.ReadKey();
+        }
+    }
+    abstract class Animal
+    {
+        public abstract string name { get; set; }
+
+        public abstract void Say();
+
+        public void ShowInfo()
+        {
+            Console.WriteLine(name);
+            Say();
+        }
+    }
+    class Dog : Animal
+    {
+        public string dog="Собака";
+
+        public override string name
+        {
+            get
+            {
+                return dog;
+            }
+            set
+            {
+                dog = value;
+            }
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Гав");
+        }
+    }
+    class Cat : Animal
+    {
+        public string cat="Кошка";
+
+        public override string name
+        {
+            get
+            {
+                return cat;
+            }
+            set
+            {
+                cat = value;
+            }
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Мяу");
+        }
+    }
+}
